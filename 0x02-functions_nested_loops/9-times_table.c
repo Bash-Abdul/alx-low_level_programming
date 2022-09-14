44b1a04bb0afbf396fee09adeab0f10027c56a2f
Abdulah <bashirtaiwo59@gmail.com>
2022-09-14 16:22:02 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,31 +14,29 @@ void times_table(void)
 
 	while (a <= 9)
 	{
-			b = 0;
-				while (b <= 9)
-						{
-									rep = a * b;
-											if (b == 0)
-														{
-																		_putchar('0' + rep);
-																				}
-													else if (rep < 10)
-																{
-																				_putchar(' ');
-																							_putchar('0' + rep);
-																									}
-															else
-																		{
-																						_putchar('0' + rep / 10);
-																									_putchar('0' + rep % 10);
-																											}
-																	if (b < 9)
-																				{
-																								_putchar(',');
-																											_putchar(' ');
-																													}
-																			b++;
-																				}
+		b = 0;
+		while (b <= 9)
+		{
+		    rep = a * b;
+   	 	    if (b == 0)
+		    {
+			    _putchar('0' + rep);
+																	      }						
+		    else if (rep < 10)
+		    {
+			    _putchar(' ');
+			    _putchar('0' + rep);
+		    }
+		    else
+		    {
+			    _putchar('0' + rep / 10);												      _putchar('0' + rep % 10);
+		    }
+
+		    if (b < 9)														      {
+	    		    _putchar(',');		
+			    _putchar(' ');												      }									
+	    	    b++;
+		}
 					_putchar('\n');
 						a++;
 	}
