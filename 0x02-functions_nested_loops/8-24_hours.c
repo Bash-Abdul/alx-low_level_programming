109950e940cd3682af0866412750c79ec0325389
Abdulah <bashirtaiwo59@gmail.com>
2022-09-14 15:57:16 -0700

    24 hrs..

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of the day of Jack Bauer
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int hr1;
+
+	int hr2;
+
+	int mn1;
+
+	int mn2;
+
+	int a = 9;
+
+	hr2 = 0;
+
+	while (hr2 <= 2)
+	{
+		if (hr2 == 2)
+		{
+			a = 3;
+		}
+
+		hr1 = 0;
+
+		while (hr1 <= a)
+		{
+			mn 2 = 0;
+
+			while (mn2 <= 5)
+			{
+				mn1 = 0;
+
+				while (mn1 <= 9)
+				{
+					_putchar('0' + hr2);
+					_putchar('0' + hr1);
+					_putchar(':');
+					_putchar('0' + mn2);
+					_putchar('0' + mn1);
+
+					_putchar('\n');
+
+					mn1++;
+				}
+				mn2++;
+			}
+			hr1++;
+		}
+		hr2++;
+	}
+}
+
+
+
