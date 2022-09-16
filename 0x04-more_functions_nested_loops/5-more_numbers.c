6a314fa89d63d133627efc63949372eb97cff05a
Abdulah <bashirtaiwo59@gmail.com>
2022-09-16 15:47:08 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	int first;
+
+	int second;
+
+	int result;
+
+	i = 0;
+	result = 0;
+
+	while (i < 10)
+	{
+		while (result <= 14)
+		{
+			if (result < 10)
+			{
+				second = result;
+			}
+			else
+			{
+				first = result / 10;
+				second = result % 10;
+				_putchar (first + '0');
+			}
+			_putchar(second + '0');
+			result++;
+		}
+		i++;
+		result = 0;
+		_putchar ('\n');
+	}
+}
