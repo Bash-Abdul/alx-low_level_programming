5a0d1f95b8e973acaaefa6cd8a3022ff2a7f1d08
Abdulah <bashirtaiwo59@gmail.com>
2022-10-05 05:02:16 -0700

    first one

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * argc: stuff
+ * argv: stuff
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
